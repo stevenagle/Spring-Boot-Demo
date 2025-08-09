c1849d4992aaa9a127dd9944228c85a1e94e5cbb
Steven Nagle <suppressed@avoidingbots.com>
2025-08-09 11:07:15 -0500

    add About page, modularize the navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import AboutPage from './pages/AboutPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* TODO - build login page */}
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar: React.FC = () => {
+    const location = useLocation();
+
+    const isActive = (path: string) => location.pathname === path;
+
+    return (
+    <nav className="navbar">
+        <div className="navbar-logo">
+        <NavLink to="/">MockPortal</NavLink>
+        </div>
+        <ul className="navbar-links">
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        </ul>
+    </nav>
+    );
+};
+
+export default Navbar;
